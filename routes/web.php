baa2658db44d9cb9766bff6dfd495defcfe98f9d
Kaiba777 <sawadogomartial222@gmail.com>
2024-03-06 17:13:53 +0000

    Ok pour la gestion admin et user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use App\Http\Controllers\AppController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +21,10 @@ Route::get('/', [AppController::class, 'index'])->name('app.index');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function(){
+    Route::get('/my-account', [UserController::class, 'index'])->name('user.index');
+});
+
+Route::middleware(['auth', 'auth.admin'])->group(function(){
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
